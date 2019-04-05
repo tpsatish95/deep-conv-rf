91d1a058f000065805f516088dbc39817091c868
tpsatish95 <tpsatish95@gmail.com>
2019-04-04 20:45:36 -0400

    Update CNN param search with analysis code

--- experiments/DeepConvRF/CNN_CIFAR.py
+++ experiments/DeepConvRF/CNN_CIFAR.py
@@ -17,7 +17,7 @@ import progressbar
 import warnings
 import copy
 from multiprocessing import cpu_count
-import torch.multiprocessing as mp
+from torch.multiprocessing import Pool
 
 plt.style.use('seaborn')
 
@@ -90,7 +90,6 @@ class SimpleCNN2Layers(nn.Module):
 
 
 def train_model(model, train_loader, test_loader, optimizer, scheduler, EPOCH, config):
-
     t0 = time.perf_counter()
 
     Loss_train = np.zeros((EPOCH,))
@@ -218,7 +217,7 @@ param_dicts = list()
 for model_id, m in enumerate(cnn_model_space):
     for l in lr_space:
         for w in weight_decay:
-            param_dict = {"model": m, "fraction_of_train_samples": TRAIN_FRACTION, "config": {
+            param_dict = {"model": copy.deepcopy(m), "fraction_of_train_samples": TRAIN_FRACTION, "config": {
                 "model": model_id, "lr": l, "weight_decay": w}, "class1": CLASS1, "class2": CLASS2}
             param_dicts.append(param_dict)
 
@@ -227,6 +226,41 @@ def cnn_train_test_unpack(args):
     return cnn_train_test(**args)
 
 
-with mp.Pool(int(cpu_count()/torch.get_num_threads())) as p:
+with Pool(int(cpu_count()/torch.get_num_threads())) as p:
     experiment_logs = p.map(cnn_train_test_unpack, param_dicts)
 np.save("grid_search_best_cnn_logs.npy", np.array(experiment_logs))
+
+
+# try
+experiment_logs = np.load("grid_search_best_cnn_logs.npy")
+test_exp = 117
+print(experiment_logs[test_exp])
+acc_train, acc_test, loss_train, loss_test, _ = cnn_train_test(
+    cnn_model_space[experiment_logs[test_exp][2]["model"]], TRAIN_FRACTION, experiment_logs[experiment_logs[test_exp]][2], CLASS1, CLASS2)
+plot(acc_train, acc_test, loss_train, loss_test)
+
+# plot all curves
+for acc_train, acc_test, loss_train, loss_test, config in experiment_logs:
+    print(config)
+    plot(acc_train, acc_test, loss_train, loss_test)
+
+model_0, model_1, model_2 = [[i for i in experiment_logs if i[4]["model"] == x] for x in [0, 1, 2]]
+
+# sort based on best test accuracy
+model_0 = sorted(model_0, key=lambda x: x[1][-1], reverse=True)
+model_1 = sorted(model_1, key=lambda x: x[1][-1], reverse=True)
+model_2 = sorted(model_2, key=lambda x: x[1][-1], reverse=True)
+
+top_n = 10
+
+for acc_train, acc_test, loss_train, loss_test, config in model_0[:top_n]:
+    print(config)
+    plot(acc_train, acc_test, loss_train, loss_test)
+
+for acc_train, acc_test, loss_train, loss_test, config in model_1[:top_n]:
+    print(config)
+    plot(acc_train, acc_test, loss_train, loss_test)
+
+for acc_train, acc_test, loss_train, loss_test, config in model_2[:top_n]:
+    print(config)
+    plot(acc_train, acc_test, loss_train, loss_test)
