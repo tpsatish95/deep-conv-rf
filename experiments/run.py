3517fb4ce14b60c9659f83ec6186145e109b4081
tpsatish95 <tpsatish95@gmail.com>
2019-04-19 22:07:44 -0400

    Add CIFAR10 5vs7

--- experiments/run.py
+++ experiments/run.py
@@ -28,12 +28,12 @@ warnings.filterwarnings("ignore")
 
 ####### CIFAR10 ########
 DATASET_NAME = "CIFAR10"
-TITLE = "Automobile (1) vs Truck(9)"
+TITLE = "Dog (5) vs Horse(7)"
 
 DATA_PATH = "./data"
-RESULTS_PATH = "results/cifar10/100_percent_data/1vs9/"
+RESULTS_PATH = "results/cifar10/100_percent_data/5vs7/"
 
-CHOOSEN_CLASSES = [1, 9]
+CHOOSEN_CLASSES = [5, 7]
 MAX_TRAIN_FRACTION = 1.0
 
 # ####### SVHN ########
