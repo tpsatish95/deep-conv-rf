8da394d2b3313d08502b16e8fdc77cb7e4c061f0
tpsatish95 <tpsatish95@gmail.com>
2019-04-22 15:19:52 -0400

    Update svhn 3vs8

--- experiments/run.py
+++ experiments/run.py
@@ -26,25 +26,25 @@ warnings.filterwarnings("ignore")
 # Settings
 ##############################################################################################################
 
-####### CIFAR10 ########
-DATASET_NAME = "CIFAR10"
-TITLE = "Automobile (1) vs Truck(9)"
-
-DATA_PATH = "./data"
-RESULTS_PATH = "results/cifar10/100_percent_data/1vs9/"
-
-CHOOSEN_CLASSES = [1, 9]
-MAX_TRAIN_FRACTION = 1.0
-
-# ####### SVHN ########
-# DATASET_NAME = "SVHN"
-# TITLE = "0 vs 9"
+# ####### CIFAR10 ########
+# DATASET_NAME = "CIFAR10"
+# TITLE = "Automobile (1) vs Truck(9)"
 #
 # DATA_PATH = "./data"
-# RESULTS_PATH = "results/svhn/0vs9/"
+# RESULTS_PATH = "results/cifar10/100_percent_data/1vs9/"
 #
-# CHOOSEN_CLASSES = [0, 9]
+# CHOOSEN_CLASSES = [1, 9]
 # MAX_TRAIN_FRACTION = 1.0
+
+####### SVHN ########
+DATASET_NAME = "SVHN"
+TITLE = "3 vs 8"
+
+DATA_PATH = "./data"
+RESULTS_PATH = "results/svhn/3vs8/"
+
+CHOOSEN_CLASSES = [3, 8]
+MAX_TRAIN_FRACTION = 1.0
 
 # ####### FashionMNIST ########
 # DATASET_NAME = "FashionMNIST"
